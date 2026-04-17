db1d83b6906c7a56242d84974324e866b2454ca0
Dhusyanth <dhusyanth@example.com>
2026-04-17 10:46:02 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
